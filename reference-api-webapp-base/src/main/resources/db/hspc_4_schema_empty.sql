200a326aff03b57d01d267c462f22c030269a194
Mike Bylund <mike@isalussolutions.com>
2017-03-24 12:46:47 -0600

    Changed starter data to lower case so it will work on AWS.
    Former-commit-id: 1b418022ad7eb9076c0023894f51e1c20303da1e

--- reference-api-webapp-base/src/main/resources/db/hspc_4_schema_empty.sql
+++ reference-api-webapp-base/src/main/resources/db/hspc_4_schema_empty.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cd73759fce9a03ef0b08a3c160f19c2a0ff34fd4124aa87205b6f4cd1629d141
-size 26945
+oid sha256:f8c38da9f3613f02486d44e01537ff63fc627099a8ab8bb993832d0aec2fb63b
+size 26946
