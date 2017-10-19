8aa7b0734822835a0191f9a46793c42f097f447e
Mike B <cmikeb1@gmail.com>
2017-10-18 18:04:58 -0600

    Updated default schema .sql
    Former-commit-id: b90472d319d140e368f3724c28490e2c2864c390
    Former-commit-id: d421595a486235e9bde0fd7693fffd31212ead47

--- reference-api-webapp-base/src/main/resources/db/hspc_5_stu3_default_dataset.sql
+++ reference-api-webapp-base/src/main/resources/db/hspc_5_stu3_default_dataset.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:90e1ec5d481455b7a318f9a4f5c5391e95c396bbfdb2ed9bd5307865fc29eb26
-size 46607
+oid sha256:56013203a53688a5c82e066bf8029a58b7ed3f894b592e209323d280d1218bd6
+size 37393
