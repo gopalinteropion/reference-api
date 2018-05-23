f046b19a30b323e0ee3f0c0070094a0fcfc37b89
Travis Cummings <travis@interopion.com>
2018-05-23 15:50:24 -0600

    Fixed migration script
    Former-commit-id: 053f23f5bea7296375bb510905814582afa83679
    Former-commit-id: 887d9239af1c0c436f66d280ee231841fb1161bf

--- reference-api-mysql/src/main/resources/db/mysql/V1.1__for_all_hspc_5_schemas.sql
+++ reference-api-mysql/src/main/resources/db/mysql/V1.1__for_all_hspc_5_schemas.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:13d55452e14b56a54217f35d853db3822da78b42599e1681e636c307ec6ef126
-size 2071
+oid sha256:ba7b039719d7e0dde04b7947f9ec06e053217bd0b09f6cc31da756f7d1d90461
+size 2070
