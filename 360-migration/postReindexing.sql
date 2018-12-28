cc7c66344451bbb477811a086dab7cde5cec0b9a
Jacob Crump <>
2018-12-28 15:42:31 -0700

    Working on finalizing migration script to 3.6.0
    Former-commit-id: a12479cd4cda4f36a89df5e3afc78c4b02a78c4c
    Former-commit-id: 8716137a7f74e4be7d74e4bd92bbd22688b25d35

--- 360-migration/postReindexing.sql
+++ 360-migration/postReindexing.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eb3619bb7215d75fb5dc4e84a526527ada53a95f55ded8bb460e6ab88c112516
-size 2540
+oid sha256:dcf1b1d4e60eab78338c72602655c22bf26490a541db8cbc2de03d0125f1038f
+size 4559
