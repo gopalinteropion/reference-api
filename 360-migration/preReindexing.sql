8efaa12170a62c4872469752cc3a08f94e30d7ab
Jacob Crump <jacob@interopion.com>
2019-02-19 15:28:03 -0700

    Adding script to perform migration on all sandboxes.
    Former-commit-id: ff72af479ed0a236514b99959572a1ad5d48948f
    Former-commit-id: 26b653aa8a5643169a71b8dea0adec2f1b6f4bbd

--- 360-migration/preReindexing.sql
+++ 360-migration/preReindexing.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a2967aedce8df8356dba624055d1966165ab549b00e362c5ef2a80e74498dec2
-size 3476
+oid sha256:a8256ad3a8045064db5bcda9e20adf6ff3a6251a37ce178c7ca7e303fa1e996e
+size 3474
