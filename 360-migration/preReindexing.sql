d690e83ab3bd0bf33e943b6297597a1bfc19010a
Jacob Crump <jacob@interopion.com>
2019-02-19 10:02:29 -0700

    Final fixes for 3.7.0. Everything seems to be working, more tests pending.
    Former-commit-id: 9e9bbcf0f59d45c450d07987018a5432092fb8e3
    Former-commit-id: 8ba194fdb3f7d928ad67892904c760532dabf811

--- 360-migration/preReindexing.sql
+++ 360-migration/preReindexing.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d3e52948bb78a70cd9efe41fd6945ab7931383cf10b10642e562b2368302a18f
-size 3404
+oid sha256:a2967aedce8df8356dba624055d1966165ab549b00e362c5ef2a80e74498dec2
+size 3476
