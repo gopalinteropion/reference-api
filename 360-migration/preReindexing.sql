cf6964a5f64159971042e5c6fe632bd3bcb92f14
Jacob Crump <jacob@interopion.com>
2019-02-20 08:12:04 -0700

    Working on scripts.
    Former-commit-id: 7050de26a9da0e377f14e718d735939f565d3876
    Former-commit-id: 00eef987f33983d5849bc3d5e06df722500b06b9

--- 360-migration/preReindexing.sql
+++ 360-migration/preReindexing.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a8256ad3a8045064db5bcda9e20adf6ff3a6251a37ce178c7ca7e303fa1e996e
-size 3474
+oid sha256:1a4dc192f2bce0527281907fdc30a9faaf5d92e3a25f93c34740d1de1267fc28
+size 4092
