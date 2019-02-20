b94de1cc7e6cf2b15392be0bc3bd98696a19e082
Jacob Crump <jacob@interopion.com>
2019-02-20 11:50:16 -0700

    Making sql script dynamic.
    Former-commit-id: c546e39579e38de7fd9ef3841ba19a2c530c9a63
    Former-commit-id: da84bd7490b1917eab75e34e6b0584f8a0705e20

--- 360-migration/preReindexing.sql
+++ 360-migration/preReindexing.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1a4dc192f2bce0527281907fdc30a9faaf5d92e3a25f93c34740d1de1267fc28
-size 4092
+oid sha256:36b06591f25c5daa992d8c4d5a96adcac85d4685ef00f5412ec53578bde45128
+size 5329
