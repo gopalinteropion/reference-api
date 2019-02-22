9533788ca45f9a30e4d551e5fe3c1b80d72fe1da
Jacob Crump <jacob@interopion.com>
2019-02-22 11:35:12 -0700

    Cqf-ruler stuff.
    Former-commit-id: 5ad4e1d2876e94bece1a06223b23a127ed4580e3
    Former-commit-id: 25306af3c1c25642b51bbc5c678d5b2d00131f3a

--- 360-migration/preReindexing.sql
+++ 360-migration/preReindexing.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a72630e4a665b57d2471ae59dac52e5180fc2fcec0e8298863d19b77165996af
-size 5539
+oid sha256:82eccd905299f97aa6850c02f009227b8592307de3fe987ac7f8501567e03d5c
+size 5526
--- reference-api-mysql/src/main/resources/db/mysql/hspc_5_schema_empty.sql
+++ reference-api-mysql/src/main/resources/db/mysql/hspc_5_schema_empty.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bf58580da5e88107096051cc7512c1cffc8dd3e8373b72c7a69c19e03adcc785
-size 72312
+oid sha256:71e7cdb5f19b4270d3e3d258d2033c948cce4745249b5158ebb5a2c7bac18cf8
+size 73022
