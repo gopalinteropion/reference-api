a4c5614531de0710c8bf1c1229d7b403aaef59b1
pz12 <jkcrump12@gmail.com>
2019-03-04 15:29:32 -0700

    Finishing things up.
    Former-commit-id: f2ae9c2f2b351fd624c63c2759cae3c9e277bf8d
    Former-commit-id: 164226666bd0cfbb566a32175a212e12053bccf8

--- 360-migration/370-migration/preReindexing.sql
+++ 360-migration/370-migration/preReindexing.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d412c69ddfe5026a7c2190cc57b81a2fea1546265c7fb708a4527919d2d60f8b
-size 5076
+oid sha256:6c2db0713c6d7c92fa7e256ab25483c76b65009a43c8a86cbe94b5f71e57c402
+size 5538
--- 360-migration/370-migration/temp-dump.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d43a7a0cb5192f1cab4bc4e26141d7157c514c92186c97fbef3911c172999a84
-size 2118
