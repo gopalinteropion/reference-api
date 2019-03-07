b08627761d9e732f3a789abf874eb98f068fc93e
Jacob Crump <jacob@interopion.com>
2019-03-07 09:20:41 -0700

    Updating default scripts and run-local.sh
    Former-commit-id: 4c2637ccaa63a64b0e6c8d8b5d7fa7d0f5e84ae9
    Former-commit-id: 047aa8e55bd40d929ef52c39e72a682bd37bda6f

--- reference-api-mysql/src/main/resources/db/mysql/hspc_8_schema_empty.sql
+++ reference-api-mysql/src/main/resources/db/mysql/hspc_8_schema_empty.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:00a412e64d5ca37447d4fe15ea16ab3014af66848f19fd2f23ed1dd943714a2e
-size 73022
+oid sha256:1e9661e5815f6da9a45b56ffcd95a3e6836d3f69a2ef5596e18ba1c1a7f8b4a8
+size 55527
