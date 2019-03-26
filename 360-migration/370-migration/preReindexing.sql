724d1d1a2d46fa051da5aad76e7832e555f004ab
Jacob Crump <jacob@interopion.com>
2019-03-26 09:03:38 -0600

    Adding stuff for migrations
    Former-commit-id: 87edd7fdfe8fbfd798f28263196a9107cd28b260
    Former-commit-id: 1022ab798b702c86ed66417496baadea2bfcaa7a

--- 360-migration/370-migration/preReindexing.sql
+++ 360-migration/370-migration/preReindexing.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6c2db0713c6d7c92fa7e256ab25483c76b65009a43c8a86cbe94b5f71e57c402
-size 5538
+oid sha256:accdaf3028779c62ef82b9a3598a9351f8238ef3d72290fe3bba04afdf78f488
+size 5559
